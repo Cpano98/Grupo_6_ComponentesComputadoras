afe93322ed8e4ea17ed25a01fb2423e3e6b98a82
enoc494 <angelavelarb.94@gmail.com>
2021-12-17 14:11:52 -0600

    separe las rutas main y user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app     = express();
 
 /* bloque de routes */
 const mainRoutes = require("./routes/mainRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 /* ruta de estilos */ 
 app.use(express.static("./public"));
@@ -11,7 +12,11 @@ app.use(express.static("./public"));
 
 app.set('view engine', 'ejs');
 
+//Anclado de rutas [se llama así?]
 app.use("/",mainRoutes);
+app.use("/",userRoutes);
+
+
 app.listen(3030,()=>{
     console.log("Servidor activo en 3030");
 })
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -14,18 +14,6 @@ El return con res.render permite envíar variables al ejs
 const controller = {
     index: (req, res) => { 
         return res.render("index.ejs");
-    },
-    login: (req, res) => { 
-        return res.render("login.ejs");
-    },
-    register: (req, res) => { 
-        return res.render("register.ejs");
-    },
-    productCart: (req, res) => { 
-        return res.render("productCart.ejs");
-    },
-    productDetail: (req, res) => { 
-        return res.render("productDetail.ejs");
     }
 }
 
