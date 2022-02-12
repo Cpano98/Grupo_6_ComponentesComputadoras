32989b091e61d8c7a0496fadf00e291c0c12dc34
enoc494 <angelavelarb.94@gmail.com>
2022-02-12 09:11:26 -0600

    puse cambios en el puerto para la config del database

---
+++ src/database/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "development": {
+    "username": "u699789424_MiPCuser",
+    "password": "c5XlUeB0];",
+    "database": "u699789424_MiPCproject",
+    "host": "151.106.116.113",
+    "dialect": "mysql",
+    "port":"3306",
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
