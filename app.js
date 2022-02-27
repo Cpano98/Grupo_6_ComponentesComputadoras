015e347d85a4ba0f9650158be6ca687658a1abeb
enoc494 <angelavelarb.94@gmail.com>
2022-02-27 12:47:05 -0600

    vistas funcionales en sus carpetas

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // ************ Require's ************
-const createError = require('http-errors');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const session = require('express-session');
-const logger = require('morgan');
-const path = require('path');
-const methodOverride = require('method-override'); // Pasar poder usar los métodos PUT y DELETE
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const session = require("express-session");
+const logger = require("morgan");
+const path = require("path");
+const methodOverride = require("method-override"); // Pasar poder usar los métodos PUT y DELETE
 
 // ************ express() - (don't touch) ************
 const app = express();
@@ -14,22 +14,23 @@ app.use(express.urlencoded({ extended: true }));
 //app.use(logger('dev'));
 //app.use(express.json());
 app.use(cookieParser());
-app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
+app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 //app.use(express.bodyParser());
-app.use(session({
-    secret: '10DeCarnazaParaLlevar',
+app.use(
+  session({
+    secret: "10DeCarnazaParaLlevar",
     resave: false,
     saveUninitialized: false,
     //cookie: { secure: true } //Session debe indicar cookies, no cookies default
-}))
+  })
+);
 app.use(express.json());
-
 
 /* bloque de routes */
 const mainRoutes = require("./src/routes/mainRoutes");
 const userRoutes = require("./src/routes/userRoutes");
 const productRoutes = require("./src/routes/productRoutes");
-const purchaseRoutes = require("./src/routes/purchaseRoutes")
+const purchaseRoutes = require("./src/routes/purchaseRoutes");
 
 /* bloque de middlewares globales  */
 const loggedMiddle = require("./src/middlewares/loggedMiddle");
@@ -38,8 +39,16 @@ const loggedMiddle = require("./src/middlewares/loggedMiddle");
 app.use(express.static("./src/public"));
 
 /* definiciones default de direcciones de directorios está en express */
-app.set('views', path.join( __dirname + "/src/views" ) )
-app.set('view engine', 'ejs');
+//USO DE SUBFOLDERS:
+app.set("views", [
+  path.join(__dirname + "/src/views"),
+  path.join(__dirname + "/src/views/admin"),
+  path.join(__dirname + "/src/views/products"),
+  path.join(__dirname + "/src/views/purchase"),
+  path.join(__dirname + "/src/views/users"),
+]);
+
+app.set("view engine", "ejs");
 
 //Anclado de rutas [se llama así?]
 app.use("/", mainRoutes);
@@ -51,7 +60,6 @@ app.use("/cart", purchaseRoutes);
 app.use(loggedMiddle);
 
 //Heroku Config
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Servidor activo en 3000");
-})
-    
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Servidor activo en 3000");
+});
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,87 +1,124 @@
 const express = require("express");
-const router  = express.Router();
-const multer  = require('multer');
-const path    = require('path');
-const {body} = require('express-validator'); 
+const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const { body } = require("express-validator");
 const guestMiddle = require("../middlewares/guestMiddle");
 const adminMiddle = require("../middlewares/adminMiddle");
-const userController =require("../controllers/userController");
+const userController = require("../controllers/userController");
 
 //Implementado de multer para usuario
 const storage = multer.diskStorage({
-    destination:(req, flie, cb)=>{
-        cb( null, path.join(__dirname, '../public/images/users' ) );
-    },
-    filename:(req, file, cb )=>{
-        cb( null, file.originalname );
-    }
-})
-const upload = multer({storage});
+  destination: (req, flie, cb) => {
+    cb(null, path.join(__dirname, "../public/images/users"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 //Validaciones del registro de usuarios
 const validationReg = [
-    body('name')
-        .notEmpty().withMessage('Ingrese un nombre').bail()
-        .isLength({min:5}).withMessage('Al menos 5 caracteres').bail(),
-    body('username')
-        .notEmpty().withMessage('Ingrese un nick').bail()
-        .isLength({min:5}).withMessage('Al menos 5 caracteres').bail(),
-    body('email')
-        .notEmpty().withMessage('Ingrese un correo valido').bail()
-        .isEmail().withMessage('Debe ingresar un Email valido').bail(),
-    body('password')
-        .notEmpty().withMessage('Ingrese una contraseña').bail()
-        .isLength({min:5}).withMessage('Al menos 5 caracteres').bail(),
-    body('passwordVal')
-        .notEmpty().withMessage('Ingrese su contraseña nuevamente').bail()
-        .custom( 
-            (value, {req} ) => {
-                if( value !== req.body.password){   
-                    throw new Error('Contraseña no coincide');
-                }
-            return true;
-        })
-
-]
+  body("name")
+    .notEmpty()
+    .withMessage("Ingrese un nombre")
+    .bail()
+    .isLength({ min: 5 })
+    .withMessage("Al menos 5 caracteres")
+    .bail(),
+  body("username")
+    .notEmpty()
+    .withMessage("Ingrese un nick")
+    .bail()
+    .isLength({ min: 5 })
+    .withMessage("Al menos 5 caracteres")
+    .bail(),
+  body("email")
+    .notEmpty()
+    .withMessage("Ingrese un correo valido")
+    .bail()
+    .isEmail()
+    .withMessage("Debe ingresar un Email valido")
+    .bail(),
+  body("password")
+    .notEmpty()
+    .withMessage("Ingrese una contraseña")
+    .bail()
+    .isLength({ min: 5 })
+    .withMessage("Al menos 5 caracteres")
+    .bail(),
+  body("passwordVal")
+    .notEmpty()
+    .withMessage("Ingrese su contraseña nuevamente")
+    .bail()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Contraseña no coincide");
+      }
+      return true;
+    }),
+];
 //Validaciones de la edición de datos de usuario
 const validationEdit = [
-    body('name')
-        .notEmpty().withMessage('Ingrese un nombre').bail()
-        .isLength({min:5}).withMessage('Al menos 5 caracteres').bail(),
-    body('username')
-        .notEmpty().withMessage('Ingrese un nick').bail()
-        .isLength({min:5}).withMessage('Al menos 5 caracteres').bail(),
-    body('email')
-        .notEmpty().withMessage('Ingrese un correo valido').bail()
-        .isEmail().withMessage('Debe ingresar un Email valido').bail(),
-    body('password')
-        .notEmpty().withMessage('Ingrese una contraseña').bail()
-        .isLength({min:5}).withMessage('Al menos 5 caracteres').bail(),
-    body('passwordVal').notEmpty().withMessage('Ingrese su contraseña nuevamente').bail(),
-]
+  body("name")
+    .notEmpty()
+    .withMessage("Ingrese un nombre")
+    .bail()
+    .isLength({ min: 5 })
+    .withMessage("Al menos 5 caracteres")
+    .bail(),
+  body("username")
+    .notEmpty()
+    .withMessage("Ingrese un nick")
+    .bail()
+    .isLength({ min: 5 })
+    .withMessage("Al menos 5 caracteres")
+    .bail(),
+  body("email")
+    .notEmpty()
+    .withMessage("Ingrese un correo valido")
+    .bail()
+    .isEmail()
+    .withMessage("Debe ingresar un Email valido")
+    .bail(),
+  body("password")
+    .notEmpty()
+    .withMessage("Ingrese una contraseña")
+    .bail()
+    .isLength({ min: 5 })
+    .withMessage("Al menos 5 caracteres")
+    .bail(),
+  body("passwordVal")
+    .notEmpty()
+    .withMessage("Ingrese su contraseña nuevamente")
+    .bail(),
+];
 
 const validationLog = [
-    body('password').notEmpty().withMessage('Debe incluir una contraseña').bail()
-]
-
-
+  body("password").notEmpty().withMessage("Debe incluir una contraseña").bail(),
+];
 
 router.get("/login", guestMiddle, userController.login);
 router.post("/login", validationLog, userController.logger);
 
-router.get("/register", guestMiddle , userController.register);
+router.get("/register", guestMiddle, userController.register);
 router.post("/register", validationReg, userController.registed);
 
 // "profile" es a la que deberían redirigir una vez se tiene un usuario
-router.get("/profile", adminMiddle, userController.profile );
+router.get("/profile", adminMiddle, userController.profile);
 
 router.get("/profileEdit", adminMiddle, userController.profileEdit);
-router.put("/profileEdit", upload.single('image'), validationEdit , userController.profileEditUp); 
+router.put(
+  "/profileEdit",
+  upload.single("image"),
+  validationEdit,
+  userController.profileEditUp
+);
 
-router.get("/logout", adminMiddle, userController.logout );
+router.get("/logout", adminMiddle, userController.logout);
 
 //router.get (eliminar usuario, enviar a página de confirmación
 //router.delete (eliminar usuario)
 
-
-module.exports = router; 
+module.exports = router;
