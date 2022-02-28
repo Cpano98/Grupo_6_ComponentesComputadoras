7aedc31aea54f0351e37332aa9a067ae051f40d4
enoc494 <angelavelarb.94@gmail.com>
2022-02-28 01:32:50 -0600

    la mayoría de identaciones y nomenclaturas solucionadas, salvo nombres de vistas

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,117 +1,112 @@
-const fs = require('fs');
-const path = require('path');
-const { validationResult } = require("express-validator")
+const fs = require("fs");
+const path = require("path");
+const { validationResult } = require("express-validator");
 
 // Sequelize requirements
-const db = require('../database/models');
+const db = require("../database/models");
 const sequelize = db.sequelize;
 const Op = db.Sequelize.Op;
 const Products = db.Product;
 
 const productController = {
-	/* - - - - - - - - LISTA PRODUCTO - - - - - - - - - */
-	list: (req, res) => {
-		const conOferta = [];
-		const sinOferta = [];
+  /* - - - - - - - - LISTA PRODUCTO - - - - - - - - - */
+  list: (req, res) => {
+    const conOferta = [];
+    const sinOferta = [];
 
-		Products.findAll().then((products) => {
+    Products.findAll()
+      .then((products) => {
+        products.forEach((p) => {
+          if (p.discount != "0") {
+            conOferta.push(p);
+          } else {
+            sinOferta.push(p);
+          }
+        });
 
-			products.forEach(p => {
-				if (p.discount != "0") {
-					conOferta.push(p);
-				} else {
-					sinOferta.push(p);
-				}
-			})
+        return res.render("todosLosProductos.ejs", { conOferta, sinOferta });
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  },
 
-			return res.render('todosLosProductos.ejs', { conOferta, sinOferta })
-		})
-			.catch(err => {
-				res.send(err)
-			})
-	},
+  /* - - - - - - - - DETALLES PRODUCTO - - - - - - - - - */
+  productDetail: (req, res) => {
+    // Sequelize Implementation
+    Products.findByPk(req.params.id)
+      .then((product) => {
+        return res.render("productDetail.ejs", { item: product });
+      })
+      .catch((err) => {
+        return res.render("error404", { status: 404, url: req.url });
+      });
+  },
+  /* Contenido de admin a product */
+  adminPanel: (req, res) => {
+    return res.render("adminPanel.ejs");
+  },
+  adminList: (req, res) => {
+    Products.findAll()
+      .then((products) => {
+        console.log(products[0].image);
+        return res.render("listaProductosCRUD.ejs", { products });
+      })
+      .catch((err) => {
+        return res.render("error404", { status: 404, url: req.url });
+      });
+  },
+  /* - - - - - - - - ADD PRODUCT - - - - - - - - - */
+  productAdd: (req, res) => {
+    return res.render("productAdd.ejs");
+  },
+  productAddUp: (req, res, next) => {
+    // Validate the add product form
+    const resultVal = validationResult(req);
+    if (!resultVal.isEmpty()) {
+      return res.render("productAdd.ejs", {
+        errors: resultVal.mapped(),
+        old: req.body,
+        oldFile: req.file,
+      });
+    }
+    const file = req.file;
+    if (!file) {
+      const error = new Error("No ha seleccionado un archivo");
+      error.httpStatusCode = 404;
+      return res.render("error404.ejs");
+    }
 
-	/* - - - - - - - - DETALLES PRODUCTO - - - - - - - - - */
-	productDetail: (req, res) => {
-		// Sequelize Implementation
-		Products.findByPk(req.params.id)
-			.then(product => {
-				return res.render("productDetail.ejs", { item: product });
-			})
-			.catch(err => {
-				return res.render('error404', { status: 404, url: req.url });
-			})
-	},
+    //console.log("Valores form " + req.body);
+    //console.log('Info del file' + file.originalname)
 
-	/* Contenido de admin a product */
-
-	adminPanel: (req, res) => {
-		return res.render("adminPanel.ejs");
-	},
-	adminList: (req, res) => {
-		Products.findAll()
-			.then(products => {
-				console.log(products[0].image);
-				return res.render('listaProductosCRUD.ejs', { products });
-			})
-			.catch(err => {
-				return res.render('error404', { status: 404, url: req.url });
-			})
-	},
-	/* - - - - - - - - ADD PRODUCT - - - - - - - - - */
-	productAdd: (req, res) => {
-		return res.render("productAdd.ejs");
-	},
-	productAddUp: (req, res, next) => {
-		// Validate the add product form
-		const resultVal = validationResult(req);
-		if (!resultVal.isEmpty()) {
-			return res.render('productAdd.ejs', {
-				errors: resultVal.mapped(),
-				old: req.body,
-				oldFile: req.file
-			})
-		}
-		const file = req.file
-		if (!file) {
-			const error = new Error('No ha seleccionado un archivo')
-			error.httpStatusCode = 404;
-			return res.render('error404.ejs')
-		}
-
-		//console.log("Valores form " + req.body);
-		//console.log('Info del file' + file.originalname)
-		
-		Products.create({
-			name: req.body.name,
-			sku: req.body.sku,
-			description: req.body.description,
-			price: req.body.price,
-			discount: req.body.discount,
-			image: file.originalname,
-			category: req.body.category,
-			brand: req.body.brand,
-			pieces: req.body.pieces
-		}).then(products => {
-	
-			res.redirect("admin/lista-productos")
-		})
-	},
-
-	/* - - - - - - - - EDITAR PRODUCTO - - - - - - - - - */
-	productEdit: (req, res) => {
-		Products.findByPk(req.params.id)
-			.then(products => {
-				return res.render('editarProducto.ejs', { item: products });
-			})
-			.catch(err => {
-				return res.render('error404', { status: 404, url: req.url });
-			})
-
-	},
-	productEditUp: async (req, res, next) => {
-		/* VALIDADOR de formulario de actualizarProducto */
-		/*
+    Products.create({
+      name: req.body.name,
+      sku: req.body.sku,
+      description: req.body.description,
+      price: req.body.price,
+      discount: req.body.discount,
+      image: file.originalname,
+      category: req.body.category,
+      brand: req.body.brand,
+      pieces: req.body.pieces,
+    }).then((products) => {
+      res.redirect("admin/lista-productos");
+    });
+  },
+  /* - - - - - - - - EDITAR PRODUCTO - - - - - - - - - */
+  productEdit: (req, res) => {
+    Products.findByPk(req.params.id)
+      .then((products) => {
+        return res.render("editarProducto.ejs", { item: products });
+      })
+      .catch((err) => {
+        return res.render("error404", { status: 404, url: req.url });
+      });
+  },
+  productEditUp: async (req, res, next) => {
+    /* VALIDADOR de formulario de actualizarProducto */
+    /*
 		const resultVal = validationResult(req);
 		if (!resultVal.isEmpty()){
 			let product = await Products.findByPk(req.params.id)
@@ -125,100 +120,98 @@ const productController = {
 		}
 		*/
 
-		const file = req.file
-		if (!file) {
-			const error = new Error('No hta seleccionado un archivo')
-			error.httpStatusCode = 400;
+    const file = req.file;
+    if (!file) {
+      const error = new Error("No hta seleccionado un archivo");
+      error.httpStatusCode = 400;
 
-			return res.render('error400.ejs')
-		}
+      return res.render("error400.ejs");
+    }
 
+    Products.update(
+      {
+        name: req.body.name,
+        sku: req.body.sku,
+        description: req.body.description,
+        price: req.body.price,
+        discount: req.body.discount,
+        image: file.originalname,
+        category: req.body.category,
+        brand: req.body.brand,
+        pieces: req.body.pieces,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    return res.render("productoActualizado.ejs");
+  },
+  /* - - - - - - - - BORRAR PRODUCTO - - - - - - - - - */
+  productDelete: (req, res) => {
+    Products.destroy({
+      where: { id: req.params.id },
+    })
+      .then(() => {
+        res.redirect("/products/eliminado");
+      })
+      .catch((err) => {
+        res.render("error404", { status: 404, url: req.url });
+      });
+  },
+  deleteConfirm: (req, res) => {
+    return res.render("productoEliminado.ejs");
+  },
+  //*
+  //Mover el carrito?
+  cart: (req, res) => {
+    return res.render("productCart.ejs");
+  },
+  // */
+  search: (req, res) => {
+    let palabraBusqueda = req.body.search;
+    console.log("Se buscó: " + palabraBusqueda);
 
-		Products.update({
-			name: req.body.name,
-			sku: req.body.sku,
-			description: req.body.description,
-			price: req.body.price,
-			discount: req.body.discount,
-			image: file.originalname,
-			category: req.body.category,
-			brand: req.body.brand,
-			pieces: req.body.pieces
-		},
-			{
-				where: { id: req.params.id }
-			});
-		return res.render("productoActualizado.ejs")
+    Products.findAll({
+      where: {
+        [Op.or]: [
+          {
+            brand: palabraBusqueda,
+          },
+          {
+            price: {
+              [Op.like]: palabraBusqueda,
+            },
+          },
+          {
+            name: {
+              [Op.like]: "%" + palabraBusqueda + "%",
+            },
+          },
+          {
+            discount: {
+              [Op.like]: palabraBusqueda,
+            },
+          },
+          {
+            pieces: {
+              [Op.like]: palabraBusqueda,
+            },
+          },
+        ],
+      },
+      order: [["price", "DESC"]],
+    })
+      .then((products) => {
+        return res.render("resultadosDeBusqueda.ejs", {
+          busqueda: palabraBusqueda,
+          products,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.render("error404", { status: 404, url: req.url });
+      });
+  },
+};
 
-	},
-
-	/* - - - - - - - - BORRAR PRODUCTO - - - - - - - - - */
-	productDelete: (req, res) => {
-
-		Products.destroy({
-			where: { id: req.params.id }
-		})
-			.then(() => {
-				res.redirect("/products/eliminado")
-			})
-			.catch(err => {
-				res.render('error404', { status: 404, url: req.url });
-			})
-
-	},
-	
-	deleteConfirm: (req, res) => {
-		return res.render("productoEliminado.ejs");
-	},
-	//*
-	//Mover el carrito?
-	cart: (req, res) => {
-		return res.render("productCart.ejs");
-	},
-	// */
-
-	search: (req, res) => {
-		let palabraBusqueda = req.body.search
-		console.log("Se buscó: " + palabraBusqueda);
-
-		Products.findAll({
-			where: {
-				[Op.or]: [
-					{
-						brand: palabraBusqueda
-					},
-					{
-						price: {
-							[Op.like]: palabraBusqueda
-						}
-					},
-					{
-						name: {
-							[Op.like]: '%' + palabraBusqueda + '%'
-						}
-					},
-					{
-						discount: {
-							[Op.like]: palabraBusqueda
-						}
-					},
-					{
-						pieces: {
-							[Op.like]: palabraBusqueda
-						}
-					}
-				]
-			},
-			order: [
-				['price', 'DESC'],
-			]
-		}).then(products => {
-			return res.render('resultadosDeBusqueda.ejs', { busqueda: palabraBusqueda, products });
-		}).catch(err => {
-			console.log(err);
-			return res.render('error404', { status: 404, url: req.url });
-		})
-	}
-}
-
-module.exports = productController
+module.exports = productController;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
-const { body } = require('express-validator');
+const multer = require("multer");
+const path = require("path");
+const { body } = require("express-validator");
 
 const productController = require("../controllers/productController");
 const guestMiddle = require("../middlewares/guestMiddle");
@@ -11,93 +11,117 @@ const adminMiddle = require("../middlewares/adminMiddle");
 //implementación de multer para imagenes ***
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../public/images/productosPrueba'));
-    },
-    filename: (req, file, cb) => {
-        /* modificar para incluir la hora si funciona? */
-        cb(null, file.originalname);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../public/images/productosPrueba"));
+  },
+  filename: (req, file, cb) => {
+    /* modificar para incluir la hora si funciona? */
+    cb(null, file.originalname);
+  },
+});
 const upload = multer({ storage });
 
 //Validaciones del registro de productos
 const validationProduct = [
-    body('name')
-        .notEmpty().withMessage('Ingrese un nombre').bail()
-        .isLength({max:30}).withMessage('Máximo 30 caracteres').bail(),
-    body('brand')
-        .notEmpty().withMessage('Debe elgir una marca').bail()
-        .custom( 
-            (value, {req} ) => {
-                if( value == "value1"){   
-                    throw new Error('OPCION DEFUALT TEST*');
-                }
-            return true;
-        }).bail(),
-    body('description')
-        .notEmpty().withMessage('Ingrese una descripción').bail()
-        .isLength({max:2000}).withMessage('Máximo 200 caracteres').bail(),
-    body('price')
-        .notEmpty().withMessage('Ingrese un precio').bail(),
-    body('piece')
-        .notEmpty().withMessage('Ingrese la cantidad').bail()
-        .isInt({min:0,max:10000}).withMessage('acotado entre 0 y 10000').bail(),
-    body('image')
-        .custom( (value, {req})=>{
-            let file = req.file
-            if(!file){
-                throw new Error('Debes subir una imagen');
-            }
-            return true
-        }).bail(),
-    body('category')
-        .notEmpty().withMessage('Debe elgir una categoria').bail()
-        .custom( 
-            (value, {req} ) => {
-                if( value == "value1"){   
-                    throw new Error('OPCION DEFUALT TEST*');
-                }
-            return true;
-        }).bail(),
-    body('sku')
-        .notEmpty().withMessage('Ingrese un sku').bail()
-        .isLength({min:5}).withMessage('Al menos 5 caracteres').bail(),
-    body('discount')
-        .isInt({min:0,max:1000}).withMessage('acotado entre 0 y 1000').bail()
-]
+  body("name")
+    .notEmpty()
+    .withMessage("Ingrese un nombre")
+    .bail()
+    .isLength({ max: 30 })
+    .withMessage("Máximo 30 caracteres")
+    .bail(),
+  body("brand")
+    .notEmpty()
+    .withMessage("Debe elgir una marca")
+    .bail()
+    .custom((value, { req }) => {
+      if (value == "value1") {
+        throw new Error("OPCION DEFUALT TEST*");
+      }
+      return true;
+    })
+    .bail(),
+  body("description")
+    .notEmpty()
+    .withMessage("Ingrese una descripción")
+    .bail()
+    .isLength({ max: 2000 })
+    .withMessage("Máximo 200 caracteres")
+    .bail(),
+  body("price").notEmpty().withMessage("Ingrese un precio").bail(),
+  body("piece")
+    .notEmpty()
+    .withMessage("Ingrese la cantidad")
+    .bail()
+    .isInt({ min: 0, max: 10000 })
+    .withMessage("acotado entre 0 y 10000")
+    .bail(),
+  body("image")
+    .custom((value, { req }) => {
+      let file = req.file;
+      if (!file) {
+        throw new Error("Debes subir una imagen");
+      }
+      return true;
+    })
+    .bail(),
+  body("category")
+    .notEmpty()
+    .withMessage("Debe elgir una categoria")
+    .bail()
+    .custom((value, { req }) => {
+      if (value == "value1") {
+        throw new Error("OPCION DEFUALT TEST*");
+      }
+      return true;
+    })
+    .bail(),
+  body("sku")
+    .notEmpty()
+    .withMessage("Ingrese un sku")
+    .bail()
+    .isLength({ min: 5 })
+    .withMessage("Al menos 5 caracteres")
+    .bail(),
+  body("discount")
+    .isInt({ min: 0, max: 1000 })
+    .withMessage("acotado entre 0 y 1000")
+    .bail(),
+];
 
 /*-- admin  */
 router.get("/admin", adminMiddle, productController.adminPanel);
 router.get("/admin/list", productController.adminList);
 
-
 /* --- Get products --- */
 router.get("/", productController.list);
-
 
 /* --- Get detalle ---*/
 router.get("/productDetail/:id/", productController.productDetail);
 
 /* --- Get/post crear producto --- */
-router.get("/create",adminMiddle, productController.productAdd);
-router.post("/create", upload.single('image'), validationProduct, productController.productAddUp);
+router.get("/create", adminMiddle, productController.productAdd);
+router.post(
+  "/create",
+  upload.single("image"),
+  validationProduct,
+  productController.productAddUp
+);
 
 /* --- Get/put editar producto --- */
-router.get('/edit/:id',adminMiddle, productController.productEdit);
-router.put('/edit/:id', upload.single('image'), validationProduct, productController.productEditUp);
+router.get("/edit/:id", adminMiddle, productController.productEdit);
+router.put(
+  "/edit/:id",
+  upload.single("image"),
+  validationProduct,
+  productController.productEditUp
+);
 
 /* --- Delete borrar producto --- */
-router.delete('/delete/:id', productController.productDelete);
+router.delete("/delete/:id", productController.productDelete);
 router.get("/delete", productController.deleteConfirm);
 
-
-//El carrito de compras va aquí??
-//router.get("/productCart", productController.cart);
-
 /* --- BÚSQUEDA DE PRODUCTOS --- */
-//router.get("/busqueda-producto", productController.busqueda);
-router.post("/busqueda-producto", productController.search);
+router.post("/search", productController.search);
 
-
-module.exports = router; 
+module.exports = router;
