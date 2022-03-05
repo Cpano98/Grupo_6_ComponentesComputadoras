0e28ff322d5187a56093ce5cea2819993e78fda9
enoc494 <angelavelarb.94@gmail.com>
2022-03-05 05:14:49 -0600

    checkbox de mostrar contraseña hecho, protipo de script en /js

--- src/public/js/viewPassword.js
+++ src/public/js/viewPassword.js
@@ -1,4 +1,15 @@
-// Permite la visualización de la contraseña tras encontrar
-// un id "passwordCheck"
+/* Permite la visualización de la contraseña tras encontrar
+ * un id "passwordCheck" donde se marca si se quiere ver o no la contraseña
+ * y un name "password" 
+ */ 
 
-document.querySelector('#passwordCheck')
+let checkBox = document.querySelector('#passwordCheck input')
+let password = document.getElementsByName('password')
+let passwordVal = document.getElementsByName('passwordVal')
+
+checkBox.addEventListener('click',(e)=>{
+  checkBox.checked ? password[0].type='text' : password[0].type='password';
+  checkBox.checked ? passwordVal[0].type='text' : passwordVal[0].type='password';
+ 
+})
+
