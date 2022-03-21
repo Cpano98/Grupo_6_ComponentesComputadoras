a2013131f158fb6f1e1ae737ef48b1e9ad5578ed
enoc494 <angelavelarb.94@gmail.com>
2022-03-21 16:30:42 -0600

    inicio de validación front+error corregido en nombre de editarProducto

---
+++ src/public/js/productValidator.js
@@ -0,0 +1,13 @@
+/* Validación FrontEnd de los formularios de productos
+ * 1.- capturar formularios con querySelector
+ * 2.- asignar eventos on focus con pequeñas instrucciones
+ * 3.- onChange al conseguir superar el error
+ * 4.- Solicitud de campos no vacios
+ * 5.- evento onSubmit
+ * 6.- prevenir el envio
+ * 7.- Tirar lista de errores en front
+ * 8.- Permirit envio si no hay errores
+ */
+
+let formulario = document.querySelector(form)
+  
