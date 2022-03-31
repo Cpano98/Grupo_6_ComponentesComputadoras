997e5aeb66f81c499d5c9ca40e71ff9ed0c751d5
Carlos Pano <carlos.pano@oracle.com>
2022-03-30 20:51:24 -0600

    cambios

--- src/public/js/validateInputsRegister.js
+++ src/public/js/validateInputsRegister.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
         var inputs = document.getElementsByTagName("input");
         for (var i = 0; i < inputs.length; i++) {
             if (inputs[i].value == "") {
-                event.preventDefault();
+                //event.preventDefault();
                 inputs[i].style.border = "1px solid red";
                 inputs[i].placeholder = "Campo vacío";
             }
