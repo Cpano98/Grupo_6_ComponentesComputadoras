4a7c38a2679d30aeb2ac83ad3c6f285265d5bff0
enoc494 <angelavelarb.94@gmail.com>
2022-04-01 15:09:38 -0600

    termometro de complejidad password en register

--- src/public/js/validateInputsRegister.js
+++ src/public/js/validateInputsRegister.js
@@ -3,14 +3,12 @@ window.addEventListener("load", function () {
   var form = document.getElementById("formRegister");
   let inputs = document.querySelectorAll("div.campo-ind");
   let boton = document.querySelector("button#boton_submit");
-
+  let password = document.querySelector("div.campo-ind#password")
+  let passwordComplexity = document.querySelector("#passwordComplexity")
+  passwordComplexity.style.display="none"
   //Nombramiento de errores de la vista:
   let erroresVista = document.querySelector("div.errores");
   erroresVista.style.display = "none";
-
-  
-	
-	
 
   inputs.forEach((campo) => {
     campo.addEventListener("focusin", (e) => {
@@ -22,6 +20,7 @@ window.addEventListener("load", function () {
       campo.style.borderColor = "#ced4da";
       campo.style.borderStyle = "solid";
       campo.style.borderWidth = "1px";
+      passwordComplexity.style.display="none"
     });
   });
 
@@ -59,4 +58,41 @@ window.addEventListener("load", function () {
       console.log("Enviando");
     }
   });
+
+  //Termometro de complejidad de password
+  password.addEventListener('input',(e)=>{
+    //Declaraciones para evaluar la complejidad
+    var strength = 0;
+    const value =password.children[1].value
+
+    new RegExp("(?=.{8,})").test(value) ? strength+= 1 : null;
+    new RegExp("(?=.*[A-Z])").test(value) ? strength+= 1 : null;
+    new RegExp("(?=.*[a-z])").test(value) ? strength+= 1 : null;
+    new RegExp("(?=.*[0-9])").test(value) ? strength+= 1 : null;
+    new RegExp("([^A-Za-z0-9])").test(value) ? strength+= 1 : null;
+    
+    passwordComplexity.style.color='white'
+    passwordComplexity.style.textAlign='center'
+    passwordComplexity.style.display="block"
+    
+    if(strength>=0){
+      passwordComplexity.textContent='Vulnerable'
+      passwordComplexity.style.backgroundColor='red'
+    }
+    if(strength>=2){
+      passwordComplexity.textContent='Débil'
+      passwordComplexity.style.backgroundColor='orange'
+    }
+    if(strength>=3){
+      passwordComplexity.textContent='Intermedia'
+      passwordComplexity.style.backgroundColor='blue'
+    }
+    if(strength>=5){
+      passwordComplexity.textContent='Fuerte'
+      passwordComplexity.style.backgroundColor='green'
+    }
+
+
+
+  })
 });
