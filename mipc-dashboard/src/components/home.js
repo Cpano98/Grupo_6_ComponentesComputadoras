7a0547ed5d40904ca0dc5ea27b389db4767d87f3
Carlos Pano <carlos.pano@oracle.com>
2022-04-01 21:01:52 -0600

    Minor fixes

--- mipc-dashboard/src/components/home.js
+++ mipc-dashboard/src/components/home.js
@@ -1,23 +1,54 @@
-import React from 'react'
+import React from "react";
 import "./styles/home.css";
-import DataCont from '../components/dataCont';
+import DataCont from "../components/dataCont";
 
 function home() {
   return (
     <div>
-        <div className='genaralInfoContainer'>
-            <div>
-                <DataCont titulo={'Hola mund saf'} descripcion={'Esta es la descripcion del componente'} valor={12}/>
-            </div>
-            <div>
-                <DataCont titulo={'Hola mund saf'} descripcion={'Esta es la descripcion del componente'} valor={12}/>
-            </div>
-            <div>
-                <DataCont titulo={'Hola mund saf'} descripcion={'Esta es la descripcion del componente'} valor={12}/>
-            </div>
+      <div className="genaralInfoContainer">
+        <div>
+          <DataCont
+            titulo={"Total de productos"}
+            descripcion={"en mi PC"}
+            valor={12}
+          />
         </div>
+        <div>
+          <DataCont
+            titulo={"Total de usuarios"}
+            descripcion={"registrados (clients)"}
+            valor={12}
+          />
+        </div>
+        <div>
+          <DataCont
+            titulo={"Total categorias"}
+            descripcion={"de productos"}
+            valor={12}
+          />
+        </div>
+      </div>
+
+      <div className="spacer-1"></div>
+
+      <div className="genaralTwoContainer">
+        <div>
+          <DataCont
+            titulo={"Total de productos"}
+            descripcion={"Esta es la descripcion del componente"}
+            valor={12}
+          />
+        </div>
+        <div>
+          <DataCont
+            titulo={"Total de usuarios"}
+            descripcion={"Esta es la descripcion del componente"}
+            valor={12}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default home
+export default home;
