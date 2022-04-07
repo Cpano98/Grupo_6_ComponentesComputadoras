be297af74c0306be5375f35d3baccfcaa1f743ae
enoc494 <angelavelarb.94@gmail.com>
2022-04-06 21:59:15 -0500

    admin panel solo accesible si loggeado

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,15 +4,17 @@ const router  = express.Router();
 
 const mainController =require("../controllers/mainController");
 
+const guestMiddle = require("../middlewares/guestMiddle");
+const adminMiddle = require("../middlewares/adminMiddle");
 
 
 router.get("/", mainController.index);
 router.get("/index", mainController.index);
 router.get("/home", mainController.index);
 /*-- admin  */
-router.get("/admin", mainController.adminPanel);
-router.get("/admin/listProducts", mainController.adminListProducts);
-router.get("/admin/listUsers", mainController.adminListUsers);
+router.get("/admin", adminMiddle, mainController.adminPanel);
+router.get("/admin/listProducts", adminMiddle, mainController.adminListProducts);
+router.get("/admin/listUsers", adminMiddle, mainController.adminListUsers);
 
 
 
